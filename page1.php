d446db9ed25a8722bd395796f64d6c8dc3602208
Scott C. Altom <S.ALTOM@labs.clark.edu>
2019-04-23 12:27:32 -0700

    updated

---
+++ page1.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Page Title</title>
+</head>
+<body>
+    <h1>This is my template page</h1>
+    <div>
+        <ol class="li">#1</ol>
+        <ol class="li">#2</ol>
+        <ol class="li">#3</ol>
+        <ol class="li">#4</ol>
+        <ol class="li">#5</ol>
+    </div>
+
+
+
+<!-- jQuery -->
+<script src="js/jquery-3.3.1.min.js"></script>
+<!-- Bootstrap JavaScript -->
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
